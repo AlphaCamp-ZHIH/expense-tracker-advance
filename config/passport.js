032aead6b352afeb653cf72da0b582e49332aa01
zhih <fufong79570@gmail.com>
2021-05-21 01:40:14 +0800

    Add feature of auth and logout

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,31 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require('bcryptjs');
 const User = require("../models/user");
 
 module.exports = () => {
   passport.use(
     new LocalStrategy(
       { usernameField: "email", passReqToCallback: true },
-      (req, email, password, done) => {}
+      (req, email, password, done) => {
+        User.findOne({email})
+        .then(user =>{
+          //沒該帳號
+          if(!user) return done(null,false);
+
+          bcrypt.compare(password,user.password)
+          .then(isMatch =>{
+            if(isMatch){
+              return done(null,user);
+            }
+            // 密碼錯誤
+            return done(null,false);  
+          }) 
+
+        })
+
+
+      }
     )
   );
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const passport = require('passport');
+const User =require('../../models/user');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 router.get("/login" ,(req, res) => res.render('login'));
@@ -8,6 +10,34 @@ router.post("/login",passport.authenticate('local',{successRedirect:'/',failureR
 
 router.get("/register", (req, res) =>res.render('register'));
 
-router.post("/register", (req, res) => {});
+router.post("/register", (req, res) => {
+  console.log(req.body)
+  const {name, email, password, confirmPassword} = req.body;
+  User.findOne({email})
+  .then(user =>{
+    console.log(user)
+    if(user){
+      return res.render('register',{user:req.body,warning_msg:"此帳號已存在"});
+    }
+    return bcrypt.genSalt(10)
+    .then(salt => bcrypt.hash(password,salt))
+    .then(hash =>{
+      User.create({
+        name,
+        email,
+        password:hash
+      })
+    })
+    .then(() => res.redirect('/users/login'))
+    .catch(e => console.log(e));
+
+  });
+
+});
+
+router.get('/logout',(req,res)=>{
+  req.logout();
+  res.redirect('/users/login');
+})
 
 module.exports = router;
